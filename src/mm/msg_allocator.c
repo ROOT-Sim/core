a864f85083e50ea17c122290f39fec67d88cde72
Andrea Piccione <piccione@diag.uniroma1.it>
2021-02-15 15:51:42 +0100

    Fixed memory manager related copyright notices
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- src/mm/msg_allocator.c
+++ src/mm/msg_allocator.c
@@ -1,28 +1,13 @@
 /**
-* @file mm/msg_allocator.c
-*
-* @brief Memory management functions for messages
-*
-* Memory management functions for messages
-*
-* @copyright
-* Copyright (C) 2008-2021 HPDCS Group
-* https://hpdcs.github.io
-*
-* This file is part of ROOT-Sim (ROme OpTimistic Simulator).
-*
-* ROOT-Sim is free software; you can redistribute it and/or modify it under the
-* terms of the GNU General Public License as published by the Free Software
-* Foundation; only version 3 of the License applies.
-*
-* ROOT-Sim is distributed in the hope that it will be useful, but WITHOUT ANY
-* WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
-* A PARTICULAR PURPOSE. See the GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License along with
-* ROOT-Sim; if not, write to the Free Software Foundation, Inc.,
-* 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * @file mm/msg_allocator.c
+ *
+ * @brief Memory management functions for messages
+ *
+ * Memory management functions for messages
+ *
+ * SPDX-FileCopyrightText: 2008-2021 HPDCS Group <rootsim@googlegroups.com>
+ * SPDX-License-Identifier: GPL-3.0-only
+ */
 #include <mm/msg_allocator.h>
 
 #include <core/core.h>
@@ -40,7 +25,6 @@ void msg_allocator_init(void)
 
 void msg_allocator_fini(void)
 {
-	log_log(LOG_TRACE, "[T %u] msg allocator fini", rid);
 	while(!array_is_empty(free_list)){
 		mm_free(array_pop(free_list));
 	}
@@ -93,7 +77,7 @@ void msg_allocator_fossil_collect(simtime_t current_gvt)
 		// xxx this could need to check the actual sending time instead
 		// of the destination time in order to avoid a pseudo memory
 		// leak for LPs which send messages distant in logical time.
-		if(current_gvt > msg->dest_t){
+		if (current_gvt > msg->dest_t) {
 			msg_allocator_free(msg);
 		} else {
 			array_get_at(free_gvt_list, j) = msg;
