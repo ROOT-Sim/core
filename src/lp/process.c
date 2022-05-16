1a6b7d6309d4c54a4434ae3f680b5b00d04e3099
Andrea Piccione <piccione@diag.uniroma1.it>
2022-05-16 02:13:23 +0200

    Improved GVT timeliness, removed redundant doc from ScheduleNewEvent()
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- src/lp/process.c
+++ src/lp/process.c
@@ -31,19 +31,6 @@ static __thread struct lp_msg *current_msg;
 #define unmark_msg_remote(msg_p) ((struct lp_msg *)(((uintptr_t)(msg_p)) - 2U))
 #define unmark_msg_sent(msg_p) ((struct lp_msg *)(((uintptr_t)(msg_p)) - 1U))
 
-/**
- * @brief API to inject a new event in the simulation
- *
- * This is a function pointer that is setup at simulation startup to point to either
- * ScheduleNewEvent_parallel() in case of a parallel/distributed simulation, or to
- * ScheduleNewEvent_serial() in case of a serial simulation.
- *
- * @param receiver The ID of the LP that should receive the newly-injected message
- * @param timestamp The simulation time at which the event should be delivered at the recipient LP
- * @param event_type Numerical event type to be passed to the model's dispatcher
- * @param payload The event content
- * @param payload_size the size (in bytes) of the event content
- */
 void ScheduleNewEvent(lp_id_t receiver, simtime_t timestamp, unsigned event_type, const void *payload,
     unsigned payload_size) {
 
