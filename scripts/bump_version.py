53eed99a0754a7adb5b80f96124d90e1c23411d0
Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>
2022-06-28 17:43:40 +0200

    Bump version and adhere to correct semver string
    - Bumped to version v3.0.0-alpha.5
    - bump_version.py has been fixed to account for the proper semver syntax
      and to deal with the leading 'v'.
    
    Signed-off-by: Alessandro Pellegrini <a.pellegrini@ing.uniroma2.it>

--- scripts/bump_version.py
+++ scripts/bump_version.py
@@ -11,36 +11,37 @@ branch_name = check_output(['git', 'branch', '--show-current']).decode().strip()
 
 # Split version in major, minor, hotfix, and tag
 version = describe_str.split("-", 1)[0]
+version = version[1:]
 tag = "-" + describe_str.split("-", 1)[1].split("-", 1)[0]
 major, minor, hotfix = map(int, version.split(".", 3))
 
 new_tag = ""
 if tag.startswith("-alpha") or tag.startswith("-beta") or tag.startswith("-rc"):
     tag_version = int(tag.split(".")[1])
-    tag = tag.split(".")[0]
-    new_tag = f"{tag}.{tag_version + 1}"
+    new_tag = tag.split(".")[0]
+    new_tag = f"{new_tag}.{tag_version + 1}"
 
 branch_type = branch_name.split("-", 1)[0]
 
 print(f"The current branch {branch_name} is of type {branch_type}")
-print(f"Current version is {major}.{minor}.{hotfix}{new_tag}")
+print(f"Current version is v{major}.{minor}.{hotfix}{tag}")
 
-# Check if we have to increment the major
-if len(sys.argv) > 1 and sys.argv[1] ==  "major":
-    major += 1
-    minor = 0
-    hotfix = 0
-elif branch_type == "release":
-    if new_tag == "":
+if new_tag == "":
+    # Check if we have to increment the major
+    if len(sys.argv) > 1 and sys.argv[1] ==  "major":
+        major += 1
+        minor = 0
+        hotfix = 0
+    elif branch_type == "release":
         minor += 1
         hotfix = 0
-elif branch_type == "hotfix":
-    hotfix += 1
-else:
-    print("Branch name doesn't tell the version should be bumped.")
-    sys.exit(1)
+    elif branch_type == "hotfix":
+        hotfix += 1
+    else:
+        print("Branch name doesn't tell the version should be bumped.")
+        sys.exit(1)
 
-print(f"Applying new version number: {major}.{minor}.{hotfix}{new_tag}. Continue? (y/n)")
+print(f"Applying new version number: v{major}.{minor}.{hotfix}{new_tag}. Continue? (y/n)")
 if sys.stdin.read(1) != 'y':
     sys.exit(0)
 
@@ -51,7 +52,7 @@ with open(build_path, "r") as f:
     cmakelists = f.read()
 
 cmakelists, sub_cnt = re.subn(r"PROJECT_VERSION .*\)",
-                               f"PROJECT_VERSION {major}.{minor}.{hotfix}{new_tag})",
+                               f"PROJECT_VERSION v{major}.{minor}.{hotfix}{new_tag})",
                                cmakelists, count=1)
 
 if sub_cnt != 1:
@@ -61,4 +62,4 @@ if sub_cnt != 1:
 with open(build_path, "w") as f:
     f.write(cmakelists)
 
-print(f"File modified successfully, version bumped to {major}.{minor}.{hotfix}{new_tag}")
+print(f"File modified successfully, version bumped to v{major}.{minor}.{hotfix}{new_tag}")
