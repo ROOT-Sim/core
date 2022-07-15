ced245c404d26be76868925b04e4b0f20da4e00c
Andrea Piccione <piccione@diag.uniroma1.it>
2022-07-15 18:22:58 +0200

    Reformat of the bitmap test
    Criticism and suggestions are always welcome!
    
    Signed-off-by: Andrea Piccione <piccione@diag.uniroma1.it>

--- test/tests/datatypes/bitmap.c
+++ test/tests/datatypes/bitmap.c
@@ -6,22 +6,19 @@
  * SPDX-FileCopyrightText: 2008-2022 HPDCS Group <rootsim@googlegroups.com>
  * SPDX-License-Identifier: GPL-3.0-only
  */
+#include <datatypes/bitmap.h>
+
+#include <test.h>
+
 #include <memory.h>
 #include <stdint.h>
 #include <stdlib.h>
-#include <stdbool.h>
-
-#include "test.h"
-
-#include "datatypes/bitmap.h"
 
 #define THREAD_REPS 100000
 #define BITMAP_ENTRIES 10000
 
 static test_ret_t bitmap_test(__unused void *_)
 {
-	int ret = 0;
-
 	size_t b_size = bitmap_required_size(BITMAP_ENTRIES);
 	block_bitmap *b = malloc(b_size);
 	bitmap_initialize(b, BITMAP_ENTRIES);
@@ -30,10 +27,10 @@ static test_ret_t bitmap_test(__unused void *_)
 	memset(b_check, 0, sizeof(*b_check) * BITMAP_ENTRIES);
 
 	unsigned i = THREAD_REPS;
-	while (i--) {
+	while(i--) {
 		unsigned e = test_random_range(BITMAP_ENTRIES);
 		bool v = (double)test_random_u() / RAND_MAX > 0.5;
-		if (v)
+		if(v)
 			bitmap_set(b, e);
 		else
 			bitmap_reset(b, e);
@@ -44,24 +41,24 @@ static test_ret_t bitmap_test(__unused void *_)
 	unsigned c = bitmap_count_set(b, b_size);
 
 	i = BITMAP_ENTRIES;
-	while (i--)
+	while(i--)
 		c -= b_check[i];
 
-	if (c)
+	if(c)
 		return -1;
 
-#define bitmap_check_test(i) 	\
-__extension__({			\
-	if (!b_check[i])	\
-		return -1;	\
-	b_check[i] = false;	\
-})
+#define bitmap_check_test(i)                                                                                           \
+	__extension__({                                                                                                \
+		if(!b_check[i])                                                                                        \
+			return -1;                                                                                     \
+		b_check[i] = false;                                                                                    \
+	})
 
 	bitmap_foreach_set(b, b_size, bitmap_check_test);
 
 	free(b);
 	free(b_check);
-	return ret;
+	return 0;
 }
 
 int main(void)
