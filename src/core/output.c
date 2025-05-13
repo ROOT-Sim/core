994261e23441b63e646ba609303e93a2813fe321
AdrianoPi <24545691+AdrianoPi@users.noreply.github.com>
2025-05-13 13:29:15 +0200

    FIX properly free committed output data upon message rollback
    Signed-off-by: AdrianoPi <24545691+AdrianoPi@users.noreply.github.com>

--- src/core/output.c
+++ src/core/output.c
@@ -10,7 +10,8 @@ extern __thread struct lp_msg *current_msg;
 void ScheduleOutput(unsigned output_type, const void *output_content, unsigned output_size)
 {
 	if(unlikely(global_config.serial)) {
-		// Chiama direttamente il dispatcher
+		// Call the dispatcher directly
+		// TODO
 		return;
 	}
 
@@ -59,6 +60,20 @@ void free_msg_outputs(output_array_t *output_array)
 
 	array_fini(*output_array);
 	mm_free(output_array);
+}
+
+void committed_output_on_rollback(struct lp_msg *msg)
+{
+	output_array_t *outputs = msg->outputs;
+	if(!outputs)
+		return;
+
+	for(array_count_t i = 0; i < array_count(*outputs); ++i) {
+		struct output_data data = array_get_at(*outputs, i);
+		mm_free(data.content);
+	}
+
+	array_count(*outputs) = 0; // Necessary to avoid double freeing the array elements.
 }
 
 void execute_outputs_batch(struct lp_msg **msg_array, array_count_t size)
--- src/lp/process.c
+++ src/lp/process.c
@@ -187,6 +187,7 @@ static inline void send_anti_messages(struct process_ctx *proc_p, array_count_t 
 		}
 
 		uint32_t f = atomic_fetch_add_explicit(&msg->flags, -MSG_FLAG_PROCESSED, memory_order_relaxed);
+		committed_output_on_rollback(msg);
 		if(!(f & MSG_FLAG_ANTI))
 			msg_queue_insert_self(msg);
 		stats_take(STATS_MSG_ROLLBACK, 1);
